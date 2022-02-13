3f2d86f3a11d54b71db4febc831400ce46154c3f
fujitayasufumi <stsi.sk.hy.ju31@gmail.com>
2022-02-13 18:09:26 +0900

    doing

--- src/index.php
+++ src/index.php
@@ -1,127 +1,34 @@
 <?php
 
-class Person
-{
-    public function __construct(
-        protected string $firstName,
-        protected string $lastName
-    ) {
-    }
+$in1 = [
+    'a' => 1,
+    'b' => 2,
+    'c' => 3,
+    'd' => 4,
+];
 
-    public function show(): void
-    {
-        echo "<p>僕の名前は{$this->lastName}{$this->firstName}です</p>";
-    }
-}
+$in2 = 99;
 
-class BusinessPerson extends Person
-{
-    public function work(): void
-    {
-        echo "{$this->lastName}{$this->firstName}は働いています。";
-    }
-}
+//////////////////////////////////
+//       SOME CODE HERE         //
+//////////////////////////////////
 
-class HetareBusinessPerson extends BusinessPerson
-{
-    public function work(): void
-    {
-        parent::work();
-        echo 'ただし、ボチボチと...';
-    }
-}
+$in1['id'] = $in2;
+$out2 = array_combine(array_map(fn ($key) => ':' . $key, array_keys($in1)), array_values($in1));
 
-class Foreigner extends Person
-{
-    public function __construct(
-        public string $firstName,
-        public string $middleName,
-        public string $lastName
-    ) {
-        parent::__construct($firstName, $lastName);
-    }
+$set = implode(', ', array_map(fn ($key) => "{$key} = :{$key}", array_keys($in1)));
 
-    public function show(): void
-    {
-        echo "<p>僕の名前は{$this->firstName}.{$this->middleName}.{$this->lastName}です";
-    }
-}
+$out1 = "UPDATE tbl SET {$set} WHERE id = :id";
 
-// (new HetareBusinessPerson('藤田', '泰史'))->work();
+var_dump($out1, $out2);
 
-class FileLogger
-{
-    private DateTime $current;
-    private SplFileObject $file;
+/*
+string 'UPDATE tbl SET a = :a, b = :b, c = :c, d = :d WHERE id = :id' (length=60)
 
-    public function __construct(
-        private string $logName
-    ) {
-        $this->current = new DateTime();
-        $this->file = new SplFileObject("{$logName}-{$this->current->format('Ymd')}.log", 'a');
-    }
-
-    public function fwrite(string $str)
-    {
-        $this->file->fwrite("[{$this->current->format('Y/m/d')}]{$str}\n");
-    }
-}
-
-// $logger = new FileLogger('log');
-// $logger->fwrite('HogeHoge Text');
-
-// $splFile = new FileLogger('log');
-// $splFile->fwrite('HogeHoge Text');
-// $splFile->fread(10);
-
-class MyParent
-{
-    public static function show()
-    {
-        echo __CLASS__;
-    }
-
-    public static function staticTest()
-    {
-        static::show();
-    }
-}
-
-class MyChild extends MyParent
-{
-    public static function show()
-    {
-        echo __CLASS__;
-    }
-}
-
-// MyChild::staticTest();
-
-/**
- * 子クラス
- * extends
- * 親クラス
- */
-
-class MyClass
-{
-    public function __construct(
-        protected string $data,
-    ) {
-    }
-
-    public function getData(): string
-    {
-        return $this->data;
-    }
-}
-
-class SubClass extends MyClass
-{
-    public function getData(): string
-    {
-        return '[' . parent::getData() . ']';
-    }
-}
-
-echo (new SubClass('あああ'))->getData();
+array (size=5)
+  ':a' => int 1
+  ':b' => int 2
+  ':c' => int 3
+  ':d' => int 4
+  ':id' => int 99
+*/
