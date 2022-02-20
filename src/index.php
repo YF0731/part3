c04d072ce784774c539d71b9dd0cc1d35626b633
fujitayasufumi <stsi.sk.hy.ju31@gmail.com>
2022-02-20 14:14:22 +0900

    doing

--- src/index.php
+++ src/index.php
@@ -1,53 +1,94 @@
 <?php
 
-// class Scanner
+// function main(int $A, int $B, int $C, int $x)
 // {
-//     private $arr = [];
-//     private $count = 0;
-//     private $pointer = 0;
-//     public function next()
-//     {
-//         if ($this->pointer >= $this->count) {
-//             $str = trim(fgets(STDIN));
-//             $this->arr = explode(' ', $str);
-//             $this->count = count($this->arr);
-//             $this->pointer = 0;
+//     $num = 0;
+//     for ($a = 0; $a <= $A; $a++) {
+//         for ($b = 0; $b <= $B; $b++) {
+//             for ($c = 0; $c <= $C; $c++) {
+//                 if ($a * 500 + $b * 100 + $c * 50 === $x) {
+//                     $num++;
+//                 }
+//             }
 //         }
-//         $result = $this->arr[$this->pointer];
-//         $this->pointer++;
-//         return $result;
 //     }
-//     public function hasNext()
-//     {
-//         return $this->pointer < $this->count;
-//     }
-//     public function nextInt()
-//     {
-//         return (int)$this->next();
-//     }
-//     public function nextDouble()
-//     {
-//         return (float)$this->next();
-//     }
+
+//     return $num;
 // }
 
-// $sc = new Scanner();
-// $first = [500, $sc->nextInt()]; // [500, 500] 3通り
-// $second = [100, $sc->nextInt()]; // [100, 100] 3通り
-// $third = [50, $sc->nextInt()]; // [50, 50] 3通り - 1
-$result = 100;
-$number = 0;
+// echo main(10, 10, 10, 2000);
 
-$tmp = [
-    500 => 2,
-    100 => 2,
-    50 => 2,
-];
+// 部分和問題
 
-foreach ($tmp as $key => $value) {
-    if ($key === 100) {
-        $number++;
+function func(int $i, int $x, &$a): bool
+{
+    // ベースケース
+    if ($i === 0) {
+        if ($x === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (func($i - 1, $x, $a)) {
+        return true;
+    }
+
+    if (func($i - 1, $x - $a[$i - 1], $a)) {
+        return true;
+    }
+
+    return false;
+}
+
+function main(): void
+{
+    $n = 5;
+    $a = [3, 5, 1, 2, 9];
+    $x = 8;
+    if (func($n, $x, $a)) {
+        echo 'Yes';
+    } else {
+        echo 'No';
     }
 }
 
-echo $number;
+main();
+
+?>
+<script>
+    const func = (i, x, a) => {
+        // ベースケース
+        if (i === 0) {
+            if (x === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (func(i - 1, x, a)) {
+            return true;
+        }
+
+        if (func(i - 1, x - a[i - 1], a)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const main = () => {
+        let n = 5;
+        let a = [3, 5, 1, 2, 9];
+        let x = 8;
+        if (func(n, x, a)) {
+            console.log('Yes');
+        } else {
+            console.log('No');
+        }
+    }
+
+    main();
+</script>
